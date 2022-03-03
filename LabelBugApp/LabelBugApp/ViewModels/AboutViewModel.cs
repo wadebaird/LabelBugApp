3f2229858ad565d9e26310207d1a5376c78f7662
Wade Baird <wadebaird@hotmail.com>
2022-03-03 10:49:38 -0600

    Remove essentials

--- LabelBugApp/LabelBugApp/ViewModels/AboutViewModel.cs
+++ LabelBugApp/LabelBugApp/ViewModels/AboutViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows.Input;
-using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace LabelBugApp.ViewModels
@@ -10,7 +9,6 @@ namespace LabelBugApp.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
         public ICommand OpenWebCommand { get; }
