83c68ac3961cc533b31780c3ce753a9f13534e90
Wade Baird <wadebaird@hotmail.com>
2022-03-03 09:41:01 -0600

    Removing unnecessary code.

--- LabelBugApp/LabelBugApp/AppShell.xaml.cs
+++ LabelBugApp/LabelBugApp/AppShell.xaml.cs
@@ -11,7 +11,6 @@ namespace LabelBugApp
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
